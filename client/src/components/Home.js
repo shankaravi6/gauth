b28f8d33a3362649d0304213e7efde9ec9764c23
shankaravi6 <shankaravi6.india@gmail.com>
2024-01-21 13:57:10 +0530

    change google login

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {useNavigate} from 'react-router-dom'
 import axios from 'axios'
+import Snackbar from '@mui/material/Snackbar'
+import { Alert } from '@mui/material'
 
 const Home = () => {
 
@@ -27,7 +29,11 @@ const Home = () => {
           "https://gauth-server-u4ww.onrender.com/send",
           sendData
         );
-        console.log(response);
+        //console.log(response);
+        if(response.data == "Email Send Successfully!"){
+        var newState = { vertical: 'top', horizontal: 'center' }
+        setState({ ...newState, openAl: true })
+        }
       } catch (error) {
         console.log(error)
       }
@@ -38,6 +44,18 @@ const Home = () => {
         navigate("/");
     } 
 
+    const [state, setState] = useState({
+      openAl: false,
+      vertical: 'top',
+      horizontal: 'center'
+    })
+    const { vertical, horizontal, openAl } = state
+  
+    const handleAlClose = () => {
+      setState({ ...state, openAl: false })
+    }
+  
+
   return (
     <div className='home'>
       <div className='card'>
@@ -45,9 +63,20 @@ const Home = () => {
         <h1>{userData?.given_name} {userData?.family_name}</h1>
         <h2 style={{color:"gray"}}>{userData?.email}</h2>
         <p style={{width:"auto", textAlign:"center"}}><span style={{fontFamily:"unset !important"}}>&#9432;</span> These details are fetched from Google authentication, your data is confident, and we don't leak your data. If you need more details, click the below button we send them through your mail</p>
-        <button type='button' className='btn' onClick={() => handleSubmit()}>Got it</button>
+        <button type='button' className='btn' onClick={() => handleSubmit()}>Get Mail</button>
         <p className='logout' onClick={logout}>Logout</p>
       </div>
+      <Snackbar
+        variant='filled'
+        anchorOrigin={{ vertical, horizontal }}
+        open={openAl}
+        autoHideDuration={2000}
+        onClose={handleAlClose}
+      >
+        <Alert onClose={handleAlClose} severity='primary' variant='filled' sx={{ width: '100%' }}>
+          Email Sent successfully!
+        </Alert>
+      </Snackbar>
     </div>
   )
 }
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,53 +1,68 @@
 import React, { useEffect } from "react";
 import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const responseGoogle = (response) => {
     var decodedHeader = jwtDecode(response.credential);
     const { sub } = decodedHeader;
-    console.log("data", decodedHeader);
-    if(sub !== null || sub !== "") {
-        navigate("/Home")
+    //console.log("data", decodedHeader);
+    if (sub !== null || sub !== "") {
+      navigate("/Home");
     }
     localStorage.setItem("userdet", JSON.stringify(decodedHeader));
   };
 
   function toggleFullScreen() {
-    if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
-     (!document.mozFullScreen && !document.webkitIsFullScreen)) {
-      if (document.documentElement.requestFullScreen) {  
-        document.documentElement.requestFullScreen();  
-      } else if (document.documentElement.mozRequestFullScreen) {  
-        document.documentElement.mozRequestFullScreen();  
-      } else if (document.documentElement.webkitRequestFullScreen) {  
-        document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
-      }  
-    } else {  
-      if (document.cancelFullScreen) {  
-        document.cancelFullScreen();  
-      } else if (document.mozCancelFullScreen) {  
-        document.mozCancelFullScreen();  
-      } else if (document.webkitCancelFullScreen) {  
-        document.webkitCancelFullScreen();  
-      }  
-    }  
+    if (
+      (document.fullScreenElement && document.fullScreenElement !== null) ||
+      (!document.mozFullScreen && !document.webkitIsFullScreen)
+    ) {
+      if (document.documentElement.requestFullScreen) {
+        document.documentElement.requestFullScreen();
+      } else if (document.documentElement.mozRequestFullScreen) {
+        document.documentElement.mozRequestFullScreen();
+      } else if (document.documentElement.webkitRequestFullScreen) {
+        document.documentElement.webkitRequestFullScreen(
+          Element.ALLOW_KEYBOARD_INPUT
+        );
+      }
+    } else {
+      if (document.cancelFullScreen) {
+        document.cancelFullScreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.webkitCancelFullScreen) {
+        document.webkitCancelFullScreen();
+      }
+    }
   }
   return (
     <div>
       <div className="full-container">
         <h1 className="title">Login With Google</h1>
-          <GoogleOAuthProvider clientId="371665581818-tgjhvkqgp2ijcln872qr22rgj3hf274u.apps.googleusercontent.com">
-            <GoogleLogin
-              onSuccess={(response) => responseGoogle(response)}
-              onError={() => console.log("Error")}
-            />
-          </GoogleOAuthProvider>
-          <p onClick={toggleFullScreen} style={{color:"#29384c", cursor:"pointer", fontWeight:"600"}}>View Full Screen</p>
+        <GoogleOAuthProvider clientId="371665581818-tgjhvkqgp2ijcln872qr22rgj3hf274u.apps.googleusercontent.com">
+          <GoogleLogin
+            onSuccess={(response) => responseGoogle(response)}
+            onError={() => console.log("Error")}
+            clientId="371665581818-tgjhvkqgp2ijcln872qr22rgj3hf274u.apps.googleusercontent.com"
+            scopes={[
+              "profile",
+              "email",
+              "https://www.googleapis.com/auth/user.phonenumbers.read",
+              "https://www.googleapis.com/auth/user.addresses.read",
+            ]}
+          />
+        </GoogleOAuthProvider>
+        <p
+          onClick={toggleFullScreen}
+          style={{ color: "#29384c", cursor: "pointer", fontWeight: "600" }}
+        >
+          View Full Screen
+        </p>
       </div>
     </div>
   );
